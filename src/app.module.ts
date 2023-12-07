1f1f4e7358bceb884295adbaa73203724defc51c
Simon Kurek <simonkurek@protonmail.com>
2023-12-07 10:22:13 +0100

    feat(GQL): configuration added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/app-config.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [
+    AppConfigModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+  ],
 })
 export class AppModule {}
