ad3f5023884b29bf2ffe17efce5bef3bb8c03843
Simon Kurek <simonkurek@protonmail.com>
2023-12-07 12:39:35 +0100

    feat(db): typeorm integration added

--- src/infrastructure/db/db.module.ts
+++ src/infrastructure/db/db.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfigModule } from 'src/config/app-config.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [AppConfigModule],
+      useFactory: (configService: ConfigService) =>
+        configService.get('database'),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [],
 })
 export class DbModule {}
